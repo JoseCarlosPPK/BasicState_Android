972c06d3185623ac39d4efaccde88b5525c927b5
JoseCarlos <jcarloslopez@correo.ugr.es>
2023-04-09 20:11:23 +0200

    (src) Add botón para incrementar el contador de vasos bebidos
    La UI no se refresca aún al pulsar el botón
    
    Signed-off-by: JoseCarlos <jcarloslopez@correo.ugr.es>

--- app/src/main/java/com/example/basicstate/WaterCount.kt
+++ app/src/main/java/com/example/basicstate/WaterCount.kt
@@ -1,6 +1,8 @@
 package com.example.basicstate
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -8,15 +10,23 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 /**
- * Muestra cuántos basos de agua te has bebido
+ * Muestra cuántos basos de agua te has bebido y proporciona un botón de incremento
  * @param modifier Permite cambiar el aspecto
  */
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val numVasos = 0
+    var numVasos = 0
 
-    Text(
-        text = stringResource(id = R.string.contador_vasos, numVasos),
-        modifier = Modifier.padding(16.dp)
-    )
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(
+            text = stringResource(id = R.string.contador_vasos, numVasos),
+        )
+        
+        Button(
+            onClick = { ++numVasos },
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Text(text = stringResource(id = R.string.suma))
+        }
+    }
 }
