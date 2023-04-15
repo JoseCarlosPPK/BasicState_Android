5e7fc302771716dca6e47a43a45c3d10a8e4b93d
JoseCarlos <jcarloslopez@correo.ugr.es>
2023-04-15 17:04:18 +0200

    (src) Add tarea saludable
    Signed-off-by: JoseCarlos <jcarloslopez@correo.ugr.es>

--- app/src/main/java/com/example/basicstate/TareaSaludable.kt
+++ app/src/main/java/com/example/basicstate/TareaSaludable.kt
@@ -2,22 +2,44 @@ package com.example.basicstate
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+
+/**
+ * Función stateful
+ */
+@Composable
+fun TareaSaludable(texto: String, modifier: Modifier = Modifier) {
+    var checkedState by remember {
+        mutableStateOf(false)
+    }
+
+    TareaSaludable(
+        texto = texto,
+        onClose = { /*TODO*/ },
+        checked = checkedState,
+        onCheckedChange = { newValue -> checkedState = newValue },
+        modifier = modifier
+    )
+}
 
 @Composable
 fun TareaSaludable(
     texto: String,
     onClose: () -> Unit,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
     modifier: Modifier = Modifier
 ) {
     Row(
@@ -31,8 +53,16 @@ fun TareaSaludable(
             text = texto
         )
 
+        Checkbox(checked = checked, onCheckedChange = onCheckedChange)
+
         IconButton(onClick = onClose) {
             Icon(Icons.Filled.Close, contentDescription = stringResource(id = R.string.cerrar_tarea))
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Visualizacion() {
+    TareaSaludable(texto = "Prueba", onClose = { /*TODO*/ }, checked = true, onCheckedChange = {})
 }
